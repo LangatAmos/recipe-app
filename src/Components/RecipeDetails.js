c5a0720b78927d6189a716f51f1669983e775ee0
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-05 23:47:00 +0300

    add recipe details component

---
+++ src/Components/RecipeDetails.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react";
+import Recipe from "./Recipe";
+import "./App.css";
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+function RecipeList({handleClick}) {
+
+    const [recipeDetails, setRecipeDetails] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/recipes/`)
+        .then((res) => res.json())
+        //.then((recipeDetails) => console.log(recipeDetails))
+        .then((recipeDetails) => setRecipeDetails(recipeDetails))
+    },[]);
+
+
+return (
+<React.Fragment>
+<h1 className="pageTitle">EXPLORE RECIPES YOU WILL LIKE</h1>
+    <div className="ui-details">
+        <div className="ui_text-wrap">
+                {recipeDetails.map((recipe, id) => {
+            return (<Recipe key={id}{...recipe} handleClick={handleClick}></Recipe>);
+            })}
+        </div>
+    </div>
+</React.Fragment>
+);
+}
+
+export default RecipeList;
