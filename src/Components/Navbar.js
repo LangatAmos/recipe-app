07120e9508ab8de60e8e868f5a216cd1745f1ca9
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-05 23:45:06 +0300

    add navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink, Link } from "react-router-dom";
+import "./App.css"
+
+
+function Navbar() {
+
+    let activeStyle = {
+        textDecoration: "underline",
+        };
+    
+        let activeClassName = "underline";
+
+
+    return (
+        <div className="navbar navbar-inverse navbar-fixed-top">
+            <h1 className="navbar-title">Recipe Search App</h1>
+            <div className='navbar-button'>
+            {/* <Link to={"/recipe-details"}>Recipe List</Link> */}
+                <button type="button" className="btn btn-signup">
+                    <NavLink to='./RecipeDetails'
+                        style={({ isActive }) =>
+                        isActive ? activeStyle : undefined }>
+                        Recipe Details
+                    </NavLink></button>
+                <button type="button" className="btn btn-signup">
+                    <NavLink to="login"
+                        className={({ isActive }) =>
+                        isActive ? activeClassName : undefined}>Log in
+                    </NavLink>
+                </button>
+                <button type="button" className="btn btn-login">
+                    <NavLink to="signup"
+                        className={({ isActive }) =>
+                        isActive ? activeClassName : undefined}>Sign Up
+                    </NavLink>
+                </button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar;
