40dce55289a9f6e6a53403dcd22080ee4149117a
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-05 23:46:14 +0300

    add a search component

---
+++ src/Components/Search.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function Search({handleSearch}) {
+  return (
+    <form className="Search">
+      <div className="form-row">
+        <div className="col-7">
+          <input
+          onChange={(e) => {handleSearch(e.target.value)}}
+            type="text"
+            className="form-control"
+            placeholder="Search recipes"
+          />
+        </div>
+      </div>
+      <div className="col-auto">
+        <button type="submit" className="btn btn-primary mb-4">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Search;
